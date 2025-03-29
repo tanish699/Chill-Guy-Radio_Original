9e7f50affbda0d136eca1d389e1cf39ae3779c41
tanish699 <tanishwadhwa2310@gmail.com>
2025-03-29 20:30:53 +0530

    Created Server

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import 'dotenv/config'
+
+//app config
+const app = express();
+const port = process.env.port || 4000;
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+// initializing routs
+
+app.get('/',(req,res)=> res.send("API Working"))
+
+app.listen(port,()=>console.log(`Server started on ${port}`))
