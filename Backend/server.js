3cda54a570867348772885e23593225de8b148df
tanish699 <tanishwadhwa2310@gmail.com>
2025-04-04 18:57:43 +0530

    adding cloudnary to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,13 @@ import cors from 'cors'
 import 'dotenv/config'
 import songRouter from './src/routes/songRoutes.js';
 import connectDb from './src/config/mongoDB.js';
+import connectCloudinary from './src/config/cloudinary.js';
 
 //app config
 const app = express();
 const port = process.env.port || 4000;
 connectDb();
+connectCloudinary();
 
 // middleware
 app.use(express.json());
